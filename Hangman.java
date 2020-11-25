4a4031d58915d4f9e536cd1003df6fcce5818452
warren yun <xxwarrenhdxx@gmail.com>
2020-11-24 19:39:55 -0500

    hangman without the cringe main method

--- Hangman.java
+++ Hangman.java
@@ -4,43 +4,6 @@ import java.util.Random;
 
 public class Hangman {
     
-    public static void main(String[] args) {
-        ArrayList<String> word = new ArrayList<String>();
-        word.add("dab");
-        word.add("swag");
-        word.add("gamer");
-        word.add("warren");
-        word.add("lolxdxd");
-
-        String phrase = "lolxdxd";
-        System.out.println(phrase);
-        String newphrase = phrase.replaceFirst("xd", "");
-        System.out.println(newphrase);
-        //System.out.println(phrase.indexOf("xd"));
-    
-        Random n = new Random();
-        System.out.println(n.nextInt(7));
-        /*
-        Scanner swag = new Scanner(System.in);
-        String bad = swag.nextLine();
-        String ree = swag.nextLine();
-        System.out.println(bad + ree);
-        swag.close();
-        
-
-        String test = "warren is swag i think";
-        System.out.println(test.indexOf("s"));
-        String finder = "swag";
-        System.out.println(test.substring(test.indexOf(finder), test.indexOf(finder) + finder.length()));
-        System.out.println(test.indexOf("dab"));
-        */
-
-        game(word);
-        
-        
-    }
-
-
     public static void game(ArrayList<String> words) {
         Random num = new Random();
         String chosenword = words.get(num.nextInt(6));
